b4deccfb8693f21671f50dd017c4b5b8f9b30ec4
jakubknejzlik <jakub.knejzlik@gmail.com>
2016-01-30 22:48:52 +0100

    first commit

---
+++ example/index.js
@@ -0,0 +1,13 @@
+var mod = angular.module('adminr-core-test',['adminr-bootstrap']);
+
+mod.config(function(AdminrDataSourcesProvider){
+    var datasource = AdminrDataSourcesProvider.createDataSource('Test','https://adminr-test-api.herokuapp.com',{supportsRangeHeader:true})
+    datasource.addResource('Me','/me')
+    datasource.addResource('User','/users/:id',{id:'@id'})
+})
+
+mod.controller('TestCtrl',function($scope,AdminrDataSources){
+    $scope.datasource = AdminrDataSources.getDataSource('Test')
+    $scope.users = $scope.datasource.getResource('User').query()
+    $scope.user = $scope.datasource.getResource('User').create()
+})
---
+++ index.coffee
@@ -0,0 +1,7 @@
+mod = angular.module('adminr-bootstrap',['adminr-datasources','ui.bootstrap'])
+
+require('./directives/pagination.coffee')
+require('./directives/panel.coffee')
+require('./directives/table.coffee')
+require('./directives/form.coffee')
+
